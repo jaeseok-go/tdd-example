20bacc2183f4505216ccd25c85b86848eb24aeff
jaeseok-go <gojeasuk2@naver.com>
2023-10-01 15:53:11 +0900

    feat: repository initialize (add gitignore)

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
